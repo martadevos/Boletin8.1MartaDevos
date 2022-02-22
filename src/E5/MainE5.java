a056ab5eb8ef913a69a72a7a406aba68282cf651
martadevos <marta.devos@iesnervion.es>
2022-02-22 12:49:07 +0100

    Acabado E5

--- src/E5/MainE5.java
+++ src/E5/MainE5.java
@@ -7,43 +7,17 @@ public class MainE5 {
     public static void main(String[] args) {
         int[] nums = new int[4];
         int[] numsIntroduc = new int[4];
-        boolean salir = false, esIgual;
+        boolean salir;
         int iguales = 0, veces=1;
-        System.out.println("Se va a iniciar el juego.");
-        System.out.println("Dispone de 12 rondas para adivinar una serie de 4 números del 0 al 9.");
-        System.out.println("Cada vez que se acierte se mostrarán tantos 0 como números se hayan acertado" +
-                ", 1 por los números cuya posición sea correcta y x en los lugares de los no acertados");
-        nums=Utilidades.rellenarArray(nums, (int)(Math.random()*10));
-        while (veces < 13 && !salir) {
-            System.out.println("Introduzca 4 números del 0 al 9");
-            numsIntroduc=Utilidades.rellenarArray(numsIntroduc, s.nextInt())
-            for (int i=0; i<4; i++) {
-                esIgual = false;
-                if (nums[i] == numsIntroduc[i]){
-                    System.out.print(1 + " ");
-                    esIgual = true;
-                    iguales++;
-                }else {
-                    for (int m : numsIntroduc) {
-                        if (nums[i] == m) {
-                            System.out.print(0 + " ");
-                            esIgual = true;
-                        }
-                    }
-                }if (!esIgual){
-                    System.out.print("x ");
-                }
-            }
-            System.out.println();
-            if (iguales == 4){
-                salir = true;
-                System.out.println("Felicidades, has acertado todos los números");
-            }
+        Utilidades.normas();
+        Utilidades.rellenarAleatorio(nums);
+        do {
+            Utilidades.rellenarTeclado(numsIntroduc);
+            iguales=Utilidades.comprobacion(iguales, nums, numsIntroduc);
+            salir=Utilidades.ganador(iguales, false);
             iguales=0;
             veces++;
-        }
-        if (veces > 12){
-            System.out.println("Se te han acabado las oportunidades, has perdido");
-        }
+        } while (veces < 13 && !salir);
+        Utilidades.perdedor(veces);
     }
 }
--- src/E5/Utilidades.java
+++ src/E5/Utilidades.java
@@ -1,11 +1,63 @@
 package E5;
 
 public class Utilidades {
-    public static int[] rellenarArray (int[] array, int n){
-        for (int i = 0; i < 4; i++){
-            array[i] = n;
-            System.out.println(array[i]+" ");
+    public static void normas(){
+        System.out.println("Se va a iniciar el juego.");
+        System.out.println("Dispone de 12 rondas para adivinar una serie de 4 números del 0 al 9.");
+        System.out.println("Cada vez que se acierte se mostrarán tantos 0 como números se hayan acertado,");
+        System.out.println("1 por los números cuya posición sea correcta y x en los lugares de los no acertados");
+    }
+
+    public static boolean ganador (int iguales, boolean salir){
+        System.out.println();
+        if (iguales == 4){
+            salir = true;
+            System.out.println("Felicidades, has acertado todos los números");
         }
-        return array;
+        return salir;
+    }
+
+    public static void perdedor (int veces){
+        if (veces > 12){
+            System.out.println("Se te han acabado las oportunidades, has perdido");
+        }
+    }
+
+    public static void rellenarAleatorio (int [] nums){
+        for (int i = 0; i < 4; i++){
+            nums[i] = (int)(Math.random()*10);
+            System.out.println(nums[i]+" ");
+        }
+    }
+
+    public static void rellenarTeclado (int [] numsIntroduc){
+        System.out.println("Introduzca 4 números del 0 al 9");
+        for (int i = 0; i < 4; i++){
+            numsIntroduc[i] = MainE5.s.nextInt();
+        }
+    }
+
+    public static boolean correcto(String i){
+        System.out.print(i);
+        return true;
+    }
+
+    public static int comprobacion(int iguales, int[] nums, int[] numsIntroduc){
+        for (int i=0; i<4; i++) {
+            boolean esIgual = false;
+            if (nums[i] == numsIntroduc[i]){
+                esIgual=correcto("1 ");
+                iguales++;
+            }else {
+                for (int m : numsIntroduc) {
+                    if (nums[i] == m) {
+                        esIgual=correcto("0 ");
+                    }
+                }
+            }if (!esIgual){
+                System.out.print("x ");
+            }
+        }
+        return iguales;
     }
 }
